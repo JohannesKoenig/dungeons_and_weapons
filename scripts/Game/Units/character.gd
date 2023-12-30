b5e71628c318c646daa59924031641ec1aa8a6bb
Johannes König <koenig.nexus@gmail.com>
2023-12-30 21:29:56 +0100

    extend character script with move to a given point

--- scripts/Game/Units/character.gd
+++ scripts/Game/Units/character.gd
@@ -1,15 +1,23 @@
 extends CharacterBody2D
 class_name Character
 
-const SPEED = 300.0
+@export var target_delta = 1.0
+
+@export var speed = 300.0
 @onready var animation_tree = $AnimationTree
 
 var direction: Vector2 = Vector2.ZERO
 var view_direction: Vector2 = Vector2(1, 0)
+var current_target_position: Vector2 = Vector2.ZERO
 
 func _physics_process(delta):
-	velocity.x = direction.x * SPEED
-	velocity.y = direction.y * SPEED
+	var current_position = get_position()
+	var current_direction = current_target_position - current_position
+	var current_distance = current_direction.length()
+	if current_distance > target_delta:
+		move(current_target_position - current_position)
+	velocity.x = direction.x * speed
+	velocity.y = direction.y * speed
 	move_and_slide()
 	update_animation()
 
@@ -17,6 +25,9 @@ func move(movement_direction: Vector2):
 	direction = movement_direction.normalized()
 	if direction != Vector2.ZERO:
 		view_direction = direction
+
+func move_to(point: Vector2):
+	current_target_position = point
 
 func update_animation():
 	animation_tree.set("parameters/Idle/blend_position", view_direction)
