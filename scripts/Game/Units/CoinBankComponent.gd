c4408f8dc23c9f9a941ef0e5417c1d4bfa1cabde
Johannes König <koenig.nexus@gmail.com>
2024-01-07 19:40:09 +0100

    add daynighttimer and update gui to work with it. Add new skins and update coin behaviour

---
+++ scripts/Game/Units/CoinBankComponent.gd
@@ -0,0 +1,14 @@
+extends Node2D
+class_name CoinBankComponent
+
+var value = 0
+
+signal value_changed(value)
+
+func add(value):
+	self.value += value
+	value_changed.emit(self.value)
+
+func remove(value):
+	self.value = max(0, self.value - value)
+	value_changed.emit(self.value)
