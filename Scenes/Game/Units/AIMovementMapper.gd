1c9d1afc11ee0a476630012852292eb7da1b7d72
Johannes König <koenig.nexus@gmail.com>
2024-01-17 07:52:49 +0100

    add wait state

--- Scenes/Game/Units/AIMovementMapper.gd
+++ Scenes/Game/Units/AIMovementMapper.gd
@@ -6,7 +6,8 @@ class_name AIMovementMapper
 @export var delta = 2
 @export var movement_stats: MovementStats
 var ai_path_markers: AiPathMarkers
-
+var timer: Timer
+var timer_started = false
 var state = null
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,6 +23,8 @@ func _process(delta):
 				_execute_movement_state(self.state)
 			"interaction":
 				_execute_interaction_state(self.state)
+			"wait":
+				_execute_wait_state(self.state)
 		
 
 func set_strategy(strategy: Dictionary):
@@ -42,23 +45,46 @@ func _execute_movement_state(movement_state: Dictionary):
 	var direction = target.global_position - actor.global_position
 	var dist = direction.length()
 	if dist <= delta:
+		actor.velocity = Vector2.ZERO
 		var next_state_name = self.state["next"]
 		if next_state_name:
 			self.state = get_state_with_name(next_state_name,self.strategy["states"])
 		else: 
 			self.state = null
 		return
-	actor.velocity = direction.normalized() * movement_stats.movement_speed
+	else:
+		actor.velocity = direction.normalized() * movement_stats.movement_speed
 
 func _execute_interaction_state(interaction_state: Dictionary):
 	actor.interact()
 	var next_state_name = self.state["next"]
-	print(next_state_name)
 	if next_state_name:
 		self.state = get_state_with_name(next_state_name,self.strategy["states"])
 	else: 
 		self.state = null
 	return
+
+func _execute_wait_state(interaction_state: Dictionary):
+	# actions
+	if timer == null:
+		timer = Timer.new()
+		add_child(timer)
+		timer.one_shot = true
+	if not timer_started:
+		if timer.is_stopped():
+			timer.start(interaction_state["parameters"]["time"])
+			timer_started = true
+	else:
+		if timer.is_stopped():
+			# pick next state
+			var next_state_name = self.state["next"]
+			if next_state_name:
+				self.state = get_state_with_name(next_state_name,self.strategy["states"])
+			else: 
+				self.state = null
+			# reset class state
+			timer_started = false
+			return
 
 func get_state_with_name(state_name: String, states: Array):
 	for _state in states:
