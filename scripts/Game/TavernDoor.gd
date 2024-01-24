de51a8bcab855469fe47588e556b5d9029bb463e
Johannes König <koenig.nexus@gmail.com>
2024-01-24 07:19:41 +0100

    rework tavern collider and occluder and make door open and close automatically

--- scripts/Game/TavernDoor.gd
+++ scripts/Game/TavernDoor.gd
@@ -1,7 +1,14 @@
 extends Node2D
 class_name TavernDoor
 
+@export var door_close_delay = 5
 var is_open: bool = false
+var timer: Timer
+
+func _ready():
+	timer = Timer.new()
+	timer.autostart = false
+	add_child(timer)
 
 func open():
 	is_open = true
@@ -15,3 +22,14 @@ func _on_actionable_action(message: Dictionary):
 		close()
 	else:
 		open()
+
+
+func _on_area_2d_body_entered(body):
+	timer.stop()
+	open()
+
+
+func _on_area_2d_body_exited(body):
+	timer.start(door_close_delay)
+	await timer.timeout
+	close()
