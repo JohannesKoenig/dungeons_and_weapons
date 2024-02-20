58cd996d7f6540ca8bc5188148df3f7837279f60
Johannes König <koenig.nexus@gmail.com>
2024-02-20 07:24:02 +0100

    increase version!

--- audio/background_music_player.gd
+++ audio/background_music_player.gd
@@ -1,4 +1,4 @@
-class_name BackgroundMusicPlayer extends AudioStreamPlayer2D
+class_name BackgroundMusicPlayer extends AudioStreamPlayer
 # ------------------------------------------------------------------------------
 # Variables ====================================================================
 # ------------------------------------------------------------------------------
